f148124ba03f2f733eab0ac0efd13c853f50432a
A7F <rockthisway05@gmail.com>
2021-06-02 22:57:35 +0200

    more datasets

--- scripts/make-points.py
+++ scripts/make-points.py
@@ -1,9 +1,10 @@
 import os.path
 import random
+import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters to consider when generating the file
-tot_points = 300000
+tot_points = 200000
 min_val = -10000
 max_val = 10000
 filepath = f"../input/{tot_points}points.txt"
@@ -20,14 +21,14 @@ if os.path.isfile(filepath):
         for line in lines:
             line = line.strip()
             x, y = line.split(" ")
-            if (int(x), int(y)) not in points:
-                points.append((int(x), int(y)))
+            if (float(x), float(y)) not in points:
+                points.append((float(x), float(y)))
 else:
     with open(filepath, "w") as fin:
         for i in range(tot_points):
             while True:
-                x = random.randint(min_val, max_val)
-                y = random.randint(min_val, max_val)
+                x = round(random.uniform(min_val, max_val), 3)
+                y = round(random.uniform(min_val, max_val), 3)
                 if (x, y) not in points:
                     points.append((x, y))
                     fin.write(f"{x} {y}\n")
